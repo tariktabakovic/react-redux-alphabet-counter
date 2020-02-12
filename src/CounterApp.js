3a819a135ad5302231a40e4a4b809ba50263ed6f
tariktabakovic <tariktab97@gmail.com>
2020-02-12 13:28:46 -0500

    implements Couter and Provider into my CounterApp

---
+++ src/CounterApp.js
@@ -0,0 +1,21 @@
+import { 
+    createStore
+} from 'redux';
+
+import { 
+    count
+} from './Reducers';
+
+import React from 'react';
+import Counter from './containers/CounterContainer';
+import {Provider} from 'react-redux';
+
+const store = createStore(count);
+
+export default function CounterApp(){
+    return (
+        <Provider store = {store}>
+            <Counter />
+        </Provider>
+    )
+};
