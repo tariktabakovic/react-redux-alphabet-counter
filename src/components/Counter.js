c02e02a306a5b03e1b5d3cbb2f038a0a0b19dd23
tariktabakovic <tariktab97@gmail.com>
2020-02-12 13:35:40 -0500

    creates my Counter componenet

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Counter ({
+    count, 
+    handleIncrement,
+    handleDecrement,
+    handleReset
+}) {
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick= {handleDecrement}>+</button>
+            <button onClick= {handleIncrement}>-</button>
+            <button onClick= {handleReset}>Reset</button>
+        </div>
+    )
+}
